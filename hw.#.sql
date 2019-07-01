c7452bc807419758211a6503f8890f5751e1dc1a
Mayomh11 <49565344+Mayomh11@users.noreply.github.com>
2019-07-01 17:11:13 -0400

    Update hw.#.sql

--- hw.#.sql
+++ hw.#.sql
@@ -1,3 +1,9 @@
+DROP TABLE departments;
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE employees;
+DROP TABLE salaries;
+DROP TABLE titles;
 
 -- Create Employees Table
 CREATE TABLE employees
